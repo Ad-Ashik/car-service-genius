f80b30c286b0f90055b55da1731f907f92b73456
AIA <adashik585@gmail.com>
2022-04-16 21:30:07 +0600

    edite firebase init file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,21 +6,21 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyAaljz3EJJwrHmlyv0y0MaSmv7Ee0kII0k",
-  // authDomain: "car-service-genius.firebaseapp.com",
-  // projectId: "car-service-genius",
-  // storageBucket: "car-service-genius.appspot.com",
-  // messagingSenderId: "999216032690",
-  // appId: "1:999216032690:web:c2458554258e18b8dc92ab"
+  apiKey: "AIzaSyAaljz3EJJwrHmlyv0y0MaSmv7Ee0kII0k",
+  authDomain: "car-service-genius.firebaseapp.com",
+  projectId: "car-service-genius",
+  storageBucket: "car-service-genius.appspot.com",
+  messagingSenderId: "999216032690",
+  appId: "1:999216032690:web:c2458554258e18b8dc92ab"
 
 
 
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId
 
 };
 
